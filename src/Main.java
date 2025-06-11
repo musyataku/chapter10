5444ae5307ef4351ea8a071b3c225b55d962cdcc
gouhara takuya <241126@std.hi-joho.ac.jp>
2025-06-11 09:31:01 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
+}
